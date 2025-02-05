70be37cc4b838e6d0d252878f0de9752e6377b45
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-05 15:53:48 +0600

    setup projects environment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-center mt-36">Admin Dashboard</h1>
+    </>
+  );
+}
+
+export default App;
