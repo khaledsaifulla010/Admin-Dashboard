cc751689eb58635cc5f99c9581c7735bf33ef4c6
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-02-05 22:29:20 +0600

    added login system using google

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,26 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FcGoogle } from "react-icons/fc";
+import auth from "../../firebase/firebase.config";
+
+const googleProvider = new GoogleAuthProvider();
+
 const Login = () => {
+  const handleLoginWithGoogle = () => {
+    signInWithPopup(auth, googleProvider);
+  };
+
   return (
     <div>
-      <h1 className="mt-10">Login</h1>
+      <h1 className="text-5xl font-bold mt-24 text-center">Please Login</h1>
+
+      <div className="mt-12">
+        <button
+          onClick={handleLoginWithGoogle}
+          className="border p-2 rounded-md font-bold text-xl w-[300px] lg:ml-[430px] flex items-center gap-1 justify-center"
+        >
+          <FcGoogle /> Login With Google
+        </button>
+      </div>
     </div>
   );
 };
